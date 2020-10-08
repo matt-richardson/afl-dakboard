e1c765daab47da1638f26d8b7e36271e32307783
Matt Richardson <matt.richardson@octopus.com>
2020-10-08 22:05:17 +1100

    Tweak settings to try and get lets encrypt to work

--- src/Startup.cs
+++ src/Startup.cs
@@ -23,12 +23,12 @@ namespace afl_dakboard
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews()
-                .AddRazorRuntimeCompilation();
             services.AddLettuceEncrypt(o =>
             {
                 o.UseStagingServer = true;
             });
+            services.AddControllersWithViews()
+                    .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,16 +41,12 @@ namespace afl_dakboard
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
