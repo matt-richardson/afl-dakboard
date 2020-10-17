8b373039c369a3f7636b086e67ccfcc22cd55f22
Matt Richardson <matt.richardson@octopus.com>
2020-10-17 13:30:57 +1100

    Missed using statements

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using LettuceEncrypt;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace afl_dakboard
 {
