740eb7fd68e2061236ee52b1abac3f2308cb0807
Matt Richardson <matt.richardson@octopus.com>
2021-07-31 20:38:40 +1000

    Log the data we get from squiggle

--- src/Controllers/Repository.cs
+++ src/Controllers/Repository.cs
@@ -4,34 +4,52 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using afl_dakboard.Models;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace afl_dakboard.Controllers
 {
     public class Repository
     {
+        private readonly ILogger<Repository> _logger;
+
+        public Repository(ILogger<Repository> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task<List<Team>> GetTeams()
         {
+            var url = "https://api.squiggle.com.au/?q=teams";
+            _logger.LogInformation("Getting teams from {Url}", url);
             var httpClient = new HttpClient();
-            var json = await httpClient.GetStringAsync("https://api.squiggle.com.au/?q=teams");
+            var json = await httpClient.GetStringAsync(url);
             var response = JsonConvert.DeserializeObject<TeamsRoot>(json);
+            _logger.LogInformation("Found {Count} teams", response.teams.Count);
             return response.teams;
         }
 
         public async Task<List<Standing>> GetStandings()
         {
             var httpClient = new HttpClient();
-            var json = await httpClient.GetStringAsync("https://api.squiggle.com.au/?q=standings");
+            var url = "https://api.squiggle.com.au/?q=standings";
+            _logger.LogInformation("Getting standings from {Url}", url);
+            var json = await httpClient.GetStringAsync(url);
             var response = JsonConvert.DeserializeObject<StandingsRoot>(json);
+            _logger.LogInformation("Found {Count} games", response.standings.Count);
             return response.standings;
         }
 
         public async Task<List<Game>> GetGamesForRichmondForThisYear()
         {
             var httpClient = new HttpClient();
-            var json = await httpClient.GetStringAsync("https://api.squiggle.com.au/?q=games;year=" + DateTime.Now.Year);
+            var url = "https://api.squiggle.com.au/?q=games;year=" + DateTime.Now.Year;
+            _logger.LogInformation("Getting games for Richmond for this year from {Url}", url);
+            var json = await httpClient.GetStringAsync(url);
             var response = JsonConvert.DeserializeObject<GamesRoot>(json);
-            return response.games.Where(x => x.ateam == "Richmond" || x.hteam == "Richmond").ToList();
+            var games = response.games.Where(x => x.ateam == "Richmond" || x.hteam == "Richmond").ToList();
+            _logger.LogInformation("Found {Count} games", games.Count);
+            return games;
         }
     }
 }
